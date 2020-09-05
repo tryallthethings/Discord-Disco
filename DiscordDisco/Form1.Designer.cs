f1f65c0bb80e3954e9a6789ae2a795e2bc327a0e
Stefan <stefan.bautz@gmail.com>
2020-09-06 00:54:24 +0200

    Initial commit

---
+++ DiscordDisco/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DiscordDisco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.counterLbl = new MetroFramework.Controls.MetroLabel();
+            this.counter = new MetroFramework.Controls.MetroLabel();
+            this.startBtn = new MetroFramework.Controls.MetroButton();
+            this.stopBtn = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // counterLbl
+            // 
+            this.counterLbl.AutoSize = true;
+            this.counterLbl.Location = new System.Drawing.Point(12, 74);
+            this.counterLbl.Name = "counterLbl";
+            this.counterLbl.Size = new System.Drawing.Size(156, 19);
+            this.counterLbl.TabIndex = 4;
+            this.counterLbl.Text = "Discord priority changed:";
+            // 
+            // counter
+            // 
+            this.counter.AutoSize = true;
+            this.counter.Location = new System.Drawing.Point(170, 74);
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(16, 19);
+            this.counter.TabIndex = 5;
+            this.counter.Text = "0";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(23, 103);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(110, 23);
+            this.startBtn.TabIndex = 6;
+            this.startBtn.Text = "Start the disco";
+            this.startBtn.UseSelectable = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Enabled = false;
+            this.stopBtn.Location = new System.Drawing.Point(169, 103);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(110, 23);
+            this.stopBtn.TabIndex = 7;
+            this.stopBtn.Text = "No more disco";
+            this.stopBtn.UseSelectable = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 138);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.counterLbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(302, 138);
+            this.MinimumSize = new System.Drawing.Size(302, 138);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "DiscordDisco";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel counterLbl;
+        private MetroFramework.Controls.MetroLabel counter;
+        private MetroFramework.Controls.MetroButton startBtn;
+        private MetroFramework.Controls.MetroButton stopBtn;
+    }
+}
+
---
+++ DiscordDisco/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DiscordDisco
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public bool discoDisco = false;
+        public int prioCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            startBtn.Enabled = false;
+            stopBtn.Enabled = true;
+            discoDisco = true;
+            Thread thread = new Thread(runTheDisco);
+            thread.Start();
+        }
+        public void runTheDisco()
+        {
+            while (discoDisco)
+            {
+                Process[] processes = Process.GetProcessesByName("Discord");
+                foreach (Process proc in processes)
+                {
+                    if (proc.PriorityClass == ProcessPriorityClass.Idle)
+                    {
+                        proc.PriorityClass = ProcessPriorityClass.High;
+                        if (proc.PriorityClass == ProcessPriorityClass.High)
+                        {
+                            prioCounter++;
+                            Random rnd = new Random();
+                            int toPlayOrNotToPlay = rnd.Next(0,2);
+                            if (toPlayOrNotToPlay == 1)
+                            {
+                                System.IO.Stream str = Properties.Resources.fart;
+                                System.Media.SoundPlayer snd = new System.Media.SoundPlayer(str);
+                                snd.Play();
+                            }
+                        }
+                    }
+                }
+                updateCounter();
+                Thread.Sleep(5000);
+            }
+        }
+
+        public void updateCounter()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke((MethodInvoker)delegate { updateCounter(); });
+                return;
+            }
+            counter.Text = prioCounter.ToString();
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            stopBtn.Enabled = false;
+            startBtn.Enabled = true;
+            discoDisco = false;
+        }
+    }
+}
